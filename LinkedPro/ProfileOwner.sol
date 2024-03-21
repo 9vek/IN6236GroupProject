810a646a9c0232bee544e299e0b04b586d14fd13
LiFangYuBIT <102638937+LiFangYuBIT@users.noreply.github.com>
2024-03-21 20:44:20 +0800

    Create ProfileOwner.sol

---
+++ LinkedPro/ProfileOwner.sol
@@ -0,0 +1,104 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >0.8.0 <0.9.0;
+
+contract ProfileOwner {
+
+    // profile owner
+    address public owner;
+
+    // other info
+
+    // credential type
+    struct Credential {
+        address issuer;
+        bytes signature;
+        bytes checksum;
+        string dataLocation;
+    }
+
+    // credential list
+    mapping(bytes32 => Credential) private credentials;
+
+    // events
+    event CredentialAdded(
+        bytes32 indexed credentialId,
+        address issuer,
+        string dataLocation
+    );
+    event CredentialSigned(bytes32 indexed credentialId, address issuer);
+    event CredentialRevoked(bytes32 indexed credentialId, address issuer);
+    event CredentialRemoved(bytes32 indexed credentialId);
+
+    // constructor
+    constructor() {
+        owner = msg.sender;
+    }
+
+    // modifiers
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Caller is not the owner");
+        _;
+    }
+
+    modifier onlyIssuer(bytes32 _credentialId) {
+        require(
+            credentials[_credentialId].issuer == msg.sender,
+            "Only issuer can sign the credential"
+        );
+        _;
+    }
+
+    // only profile owner can call to add a credential
+    function addCredential(
+        address _issuer,
+        bytes calldata _checksum,
+        string calldata _dataLocation
+    ) external onlyOwner returns (bytes32 credentialId) {
+        credentialId = keccak256(abi.encodePacked(_issuer, _checksum));
+        Credential memory newCredential = Credential({
+            issuer: _issuer,
+            signature: "",
+            checksum: _checksum,
+            dataLocation: _dataLocation
+        });
+        credentials[credentialId] = newCredential;
+        // emit ClaimAdded
+        emit CredentialAdded(credentialId, _issuer, _dataLocation);
+        return credentialId;
+    }
+
+    // issuers can sign related credentials by calling his
+    function signCredential(bytes32 _credentialId, bytes calldata _signature)
+        external
+        onlyIssuer(_credentialId)
+        returns (bool isSuccessful)
+    {
+        Credential storage credential = credentials[_credentialId];
+        credential.signature = _signature;
+        emit CredentialSigned(_credentialId, msg.sender);
+        return true;
+    }
+
+    // issuers can revoke related credentials by calling his
+    function revokeCredential(bytes32 _credentialId)
+        external
+        onlyIssuer(_credentialId)
+        returns (bool isSuccessful)
+    {
+        Credential storage credential = credentials[_credentialId];
+        credential.signature = "";
+        emit CredentialRevoked(_credentialId, msg.sender);
+        return true;
+    }
+
+    // only profile owner can call to remove a credential
+    function removeCredential(bytes32 _credentialId)
+        external
+        onlyOwner
+        returns (bool isSuccessful)
+    {
+        delete credentials[_credentialId];
+        emit CredentialRemoved(_credentialId);
+        return true;
+    }
+}
